809c516cb27ab6701a4590fe142a238924cf2247
inaz2 <inaz2@twitter>
2013-04-10 21:29:41 +0900

    max-heapを用いるように変更

--- inaz2/cut_sticks.py
+++ inaz2/cut_sticks.py
@@ -2,25 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import logging
-
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+import heapq
 
 def get_answer(sticks, cut_times, rank):
-    # 長さの降順にソート
-    sticks.sort(reverse=True)
+    # max_heapを構築
+    sticks = [(-x, x) for x in sticks]
+    heapq.heapify(sticks)
 
     for i in range(cut_times):
         # 自分の棒が取れてしまえば、あとの棒は無限小でよいとみなせるので無視できる
         # 自分の棒が確保でき、かつ、これ以上分割しても長い棒が得られないなら終了
-        if len(sticks) >= rank and sticks[0]/2.0 <= sticks[rank-1]:
+        if len(sticks) >= rank and sticks[0][1]/2.0 <= sticks[rank-1][1]:
             break
         # 一番長い棒を等分割
-        sticks[0:1] = [sticks[0]/2.0] * 2
-        sticks.sort(reverse=True)
-        logging.debug(sticks)
+        x = sticks[0][1]/2.0
+        heapq.heapreplace(sticks, (-x, x))
+        heapq.heappush(sticks, (-x, x))
+        print >>sys.stderr, [x[1] for x in sticks]
 
-    return sticks[rank-1]
+    return sticks[rank-1][1]
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
@@ -29,6 +29,7 @@ if __name__ == '__main__':
 STICKS is comma-separated values such as '1,2,3'.
 CUT_TIMES and RANK are positive integers.""" % sys.argv[0]
         sys.exit(1)
+
     sticks = map(float, sys.argv[1].split(','))
     cut_times = int(sys.argv[2])
     rank = int(sys.argv[3])
